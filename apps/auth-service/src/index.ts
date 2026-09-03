2a03fbd3c0eccdc1203ac343dbf62dfe82500490
Nguyễn Kiều Anh Quân <quansieuquay2013@gmail.com>
2026-09-03 17:39:37 +0700

    Auth service setup and package installation

---
+++ apps/auth-service/src/index.ts
@@ -0,0 +1,38 @@
+import { config } from "dotenv";
+import express from "express";
+import { resolve } from "node:path";
+import {
+  AppError,
+  errorHandler,
+  httpLogger,
+  logger,
+  successResponse,
+} from "shared";
+
+config({
+  path: resolve(process.cwd(), ".env"),
+});
+config({
+  path: resolve(process.cwd(), "../../.env"),
+});
+
+const PORT = process.env.AUTH_PORT || 3001;
+
+const app = express();
+
+app.use(httpLogger);
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  successResponse(res, { service: "auth-service" });
+});
+
+app.use((_req, res, next) => {
+  next(new AppError("Route not found", 404));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Auth service is running on port ${PORT}`);
+});
